505f9debb9cb13111dd9cea9e0d40ca0d28c39ca
Ilie Claudiu <claudiu.ilie0322@gmail.com>
2021-02-13 10:24:45 +0200

    baseline

--- src/main/java/com/home/webservices/WebServicesApplication.java
+++ src/main/java/com/home/webservices/WebServicesApplication.java
@@ -1,10 +1,13 @@
 package com.home.webservices;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebServicesApplication {
+public class WebServicesApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebServicesApplication.class, args);
